7fb21292c4fc5ec75901dece6b88930e08ec6583
Trevor Rowe <trowe@lappy.local>
2009-12-01 13:06:40 -0800

    added support for trapping and reporting fatal errors

--- Hopnote.php
+++ Hopnote.php
@@ -1,5 +1,9 @@
 <?php
 
+# TODO : provide the ability to squelch params (password, credit card, etc)
+# TODO : format params on the debug page
+# TODO : parse fatal error message into file, line and error
+# TODO : provide a better looking default fivehundred page
 class Hopnote {
 
   protected static $api_key;
@@ -7,18 +11,19 @@ class Hopnote {
   protected static $environment;
   protected static $deployed;
   protected static $errors;
-  protected static $fiveohoh;
+  protected static $fivehundred;
+  protected static $fatals;
 
   public static function register_handlers($api_key, $options = array()) {
 
-    self::$api_key = $api_key;
-
     $defaults = array(
+      'api_key'     => $api_key,
       'environment' => 'production',
-      'deployed' => true,
-      'root' => NULL,
-      'errors' => E_ALL,
-      'fiveohoh' => dirname(__FILE__) . '/500.html',
+      'deployed'    => TRUE,
+      'root'        => NULL,
+      'errors'      => E_ALL,
+      'fivehundred' => dirname(__FILE__) . '/500.html',
+      'fatals'      => FALSE,
     );
 
     foreach($defaults as $opt => $default)
@@ -27,80 +32,96 @@ class Hopnote {
       else
         self::$$opt = $default;
 
-    set_error_handler('Hopnote::error_handler', self::$errors);
-    set_exception_handler('Hopnote::exception_handler');
-  }
+    set_error_handler('Hopnote::basic_error_handler', self::$errors);
 
-  public static function error_handler($errno, $errstr) {
-    switch($errno) {
-      case E_WARNING:
-        $error_class = 'E_WARNING';
-        break;
-      case E_USER_WARNING:
-        $error_class = 'E_USER_WARNING';
-        break;
-      case E_NOTICE:
-        $error_class = 'E_NOTICE';
-        break;
-      case E_USER_NOTICE:
-        $error_class = 'E_USER_NOTICE';
-        break;
-      case E_USER_ERROR:
-        $error_class = 'E_USER_ERROR';
-        break;
-      case E_STRICT:
-        $error_class =  "E_STRICT";
-        break;
-      case E_RECOVERABLE_ERROR:
-        $error_class =  "E_RECOVERABLE_ERROR";
-        break;
-      case E_DEPRECATED:
-        $error_class =  "E_DEPRECATED";
-        break;
-      case E_USER_DEPRECATED:
-        $error_class =  "E_USER_DEPRECATED";
-        break;
+    set_exception_handler('Hopnote::exception_handler');
+      
+    if(self::$fatals) {
+      ini_set('display_errors', 'On');
+      ini_set('error_append_string', "HOPNOTE_FATAL");
+      ob_start('Hopnote::fatal_error_handler');
     }
 
-    # trigger_error inserts a funny blank entry on top of the backtrace,
-    # we want to drop it if encountered
-    $trace = debug_backtrace();
-    if(!isset($trace[0]['file']) || !isset($trace[0]['line']))
-      array_shift($trace);
+  }
 
-    self::handle_error($error_class, $errstr, $trace);
+  public static function basic_error_handler($errno, $errstr) {
+    $error_classes = array(
+      E_WARNING           => 'E_WARNING',
+      E_USER_WARNING      => 'E_USER_WARNING',
+      E_NOTICE            => 'E_NOTICE',
+      E_USER_NOTICE       => 'E_USER_NOTICE',
+      E_USER_ERROR        => 'E_USER_ERROR',
+      E_STRICT            => 'E_STRICT',
+      E_RECOVERABLE_ERROR => 'E_RECOVERABLE_ERROR',
+      E_DEPRECATED        => 'E_DEPRECATED',
+      E_USER_DEPRECATED   => 'E_USER_DEPRECATED',
+    );
+    $trace = debug_backtrace();
+    $clean_ob = true;
+    echo self::handle_error($error_classes[$errno], $errstr, $trace, $clean_ob);
+    exit;
   }
 
   public static function exception_handler($e) {
+
     $trace = $e->getTrace();
     array_unshift($trace, array(
       'file' => $e->getFile(),
       'line' => $e->getLine(),
     ));
-    self::handle_error(get_class($e), $e->getMessage(), $trace);
-  }
 
-  protected static function handle_error($error_class, $msg, $trace) {
-
-    $trace = self::parse_trace($trace);
-
-    # empty output buffers
-    $level = ob_get_level();
-    for($i = 0; $i < $level; ++$i)
-      ob_end_clean();
-
-    if(self::$deployed) {
-      self::notify_hoptoad($error_class, $msg, $trace);
-      self::display_500_page();
-    } else {
-      self::display_error_page($error_class, $msg, $trace);
-    }
+    $clean_ob = true;
+    echo self::handle_error(get_class($e), $e->getMessage(), $trace, $clean_ob);
     exit;
   }
 
+  /**
+   * fatal_error_handler is an output buffer callback.  In order to work, 
+   * it should be the first output-buffer opened.
+   */
+  public static function fatal_error_handler(&$buffer) {
+    $lines = explode("\n", $buffer);
+    $count = count($lines);
+    if($lines[$count - 1] == 'HOPNOTE_FATAL') {
+      $msg = $lines[$count - 2];
+      $trace = array();  # php won't generate a stacktrace here
+      $clean_ob = false; # calls to ob methods are not allowed here
+      $buffer = self::handle_error('FATAL', $msg, $trace, $clean_ob);
+    }
+    return $buffer;
+  }
+
+  protected static function handle_error($error_class, $msg, $trace, $clean) {
+
+    header('HTTP/1.1 500 Internal Server Error'); 
+
+    # TODO : make sure this works in & out of fatal errors
+    if($clean) {
+      $level = ob_get_level();
+      for($i = 0; $i < $level; ++$i)
+        ob_end_clean();
+    }
+
+    $trace = self::parse_trace($trace);
+
+    if(self::$deployed) {
+      self::notify_hoptoad($error_class, $msg, $trace);
+      return self::fivehundred_page();
+    } else {
+      return self::debug_page($error_class, $msg, $trace);
+    }
+  }
+
   protected static function parse_trace($backtrace) {
+
+    # trigger_error inserts a funny blank entry on top of the backtrace,
+    # we want to drop it if encountered
+    if(!isset($backtrace[0]['file']) || !isset($backtrace[0]['line']))
+      array_shift($backtrace);
+
     $root = self::$root;
     $trace = array();
+
     for($i = 0; $i < count($backtrace); ++$i) {
 
       $entry = $backtrace[$i];
@@ -126,26 +147,11 @@ class Hopnote {
     return $trace;
   }
 
-  protected static function display_error_page($error_class, $msg, $trace) {
-    ob_start();
-    # TODO : display status 500
-    include(dirname(__FILE__) . '/notice.html.php');
-    ob_end_flush();
-  }
-
   protected static function notify_hoptoad($error_class, $msg, $trace) {
-    ob_start();
-    include(dirname(__FILE__) . '/notice.xml.php');
-    $xml = ob_get_contents();
-    ob_end_clean();
-
-    echo $xml;
-    exit;
-
     $ctx = stream_context_create(array(
       'http' => array(
         'method' => 'POST',
-        'content' => $xml,
+        'content' => self::request_xml($error_class, $msg, $trace),
         'header' => "Content-type: text/xml\r\n",
       ),
     ));
@@ -154,9 +160,162 @@ class Hopnote {
       stream_get_contents($fp);
   }
 
-  protected static function display_500_page() {
-    header('HTTP/1.1 500 Internal Server Error'); 
-    include(self::$fiveohoh);
+  protected static function fivehundred_page() {
+    return file_get_contents(self::$fivehundred);
+  }
+
+  /** 
+   * Returns an HTML error page suitable for displaying in a development
+   * environment.  
+   */
+  protected static function debug_page($error_class, $msg, $trace) {
+
+    $page = array();
+
+    array_push($page, <<<EOT
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xml:lang='en' xmlns='http://www.w3.org/1999/xhtml'>
+<head>
+  <title>Error Encountered</title>
+  <meta content='text/html;charset=UTF-8' http-equiv='content-type' />
+  <style type="text/css">
+    body {
+      font-family: Arial, sans-serif;
+      padding: 0 10px;
+      margin: 0;
+    }
+    p#msg {
+      padding: 10px;
+      background-color: #ccc;
+    }
+    #trace {
+      list-style-type: none; 
+      padding: 10px; 
+      margin: 0; 
+      background-color: #ccc;
+    }
+    #trace li {
+      padding: 5px; 
+      margin: 0; 
+    }
+    .code {
+      font-family: courier, serif;
+    }
+  </style>
+</head>
+<body>
+
+  <h1>Error Encountered ($error_class)</h1>
+  <p id="msg" class="code">$msg</p>
+
+  <h2>Backtrace</h2>
+EOT
+    );
+
+    if(Hopnote::$root) {
+      $root = Hopnote::$root;
+      $p = "<p id=\"app_root\"><span>APP_ROOT:</span>";
+      $p .= Hopnote::$root;
+      $p .= '</p>';
+      array_push($page, $p);
+    }
+
+    array_push($page, '<ol id="trace" class="code">');
+    foreach($trace as $line) {
+      $function = $line['function'] ? ":in {$line['function']}" : '';
+      array_push($page, "<li>{$line['file']}:{$line['line']}$function</li>");
+    }
+    array_push($page, '</ol>');
+
+    if(isset($_REQUEST)) {
+      array_push($page, '<h2>Request</h2>');
+      array_push($page, '<div id="params"><pre>');
+      #array_push($page, print_r($_REQUEST, true));
+      array_push($page, '</pre></div>');
+    }
+
+    return implode("\n", $page);
+
+  }
+
+  protected static function request_xml($error_class, $msg, $trace) {
+
+    $xml = array();
+
+    $api_key = self::$api_key;
+    array_push($xml, '<' . '?xml version="1.0" encoding="UTF-8"?' . '>');
+    array_push($xml, '<notice version="2.0">');
+    array_push($xml, "<api-key>$api_key</api-key>");
+
+    # notifier
+    array_push($xml, '<notifier>');
+    array_push($xml, '<name>Hopnote</name>');
+    array_push($xml, '<version>1.0</version>');
+    array_push($xml, '<url>http://hopnote.lanalot.com</url>');
+    array_push($xml, '</notifier>');
+
+    # error, class, message and backtrace
+    array_push($xml, '<error>');
+    array_push($xml, "<class>$error_class</class>");
+    array_push($xml, "<message>$msg</message>");
+    if(!empty($trace)) {
+      array_push($xml, '<backtrace>');
+      foreach($trace as $t) {
+        $method = $t['function'] ? " method='{$t['function']}'" : '';
+        array_push($xml, "<line$method file='{$t['file']}' number='{$t['line']}'/>");
+      }
+      array_push($xml, '</backtrace>');
+    }
+    array_push($xml, '</error>');
+
+    # request
+    # TODO : add support for controller & action 
+    $port = $_SERVER['SERVER_PORT'] == 80 ? '' : ":{$_SERVER['SERVER_PORT']}";
+    $url = "http://{$_SERVER['HTTP_HOST']}$port{$_SERVER['REQUEST_URI']}";
+    $url = htmlentities($url);
+    array_push($xml, '<request>');
+    array_push($xml, "<url>$url</url>");
+    array_push($xml, '<component>abc</component>');
+    array_push($xml, '<action>xyz</action>');
+    if(isset($_REQUEST) && !empty($_REQUEST)) {
+      array_push($xml, '<params>');
+      foreach($_REQUEST as $k => $v) {
+        $v = htmlentities($v);
+        array_push($xml, "<var key='$k'>$v</var>");
+      }
+      array_push($xml, '</params>');
+    }
+    if(session_id() && !empty($_SESSION)) {
+      array_push($xml, '<session>');
+      foreach($_SESSION as $k => $v) {
+        $v = htmlentities($v);
+        array_push($xml, "<var key='$k'>$v</var>");
+      }
+      array_push($xml, '</session>');
+    }
+    if(isset($_SERVER) && !empty($_SERVER)) {
+      array_push($xml, '<cgi-data>');
+      foreach($_SERVER as $k => $v) {
+        $v = htmlentities($v);
+        array_push($xml, "<var key='$k'>$v</var>");
+      }
+      array_push($xml, '</cgi-data>');
+    }
+    array_push($xml, '</request>');
+
+    # server environment, project root and env name
+    $root = self::$root;
+    $env = self::$environment;
+    array_push($xml, '<server-environment>');
+    if($root)
+      array_push($xml, "<project-root>$root</project-root>");
+    array_push($xml, "<environment-name>$env</environment-name>");
+    array_push($xml, '</server-environment>');
+
+    # close the notice
+    array_push($xml, '</notice>');
+
+    return implode("\n", $xml);
   }
   
 }
